77eb9215130a6b929c571fd6ff3b409202dade4f
JKR23 <kuntualajulio7@gmail.com>
2025-07-03 18:29:30 -0400

    enroll_with_suspense

--- pages/take_it_free/TakeItFreePage.jsx
+++ pages/take_it_free/TakeItFreePage.jsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import Section1 from '@/pages/take_it_free/components/Section1';
 import Section2 from '@/pages/take_it_free/components/Section2';
 import Section3 from '@/pages/take_it_free/components/Section3';
@@ -7,10 +9,12 @@ import TokenHandler from '@/pages/take_it_free/components/TokenHandler';
 
 export default function TakeItFreePage() {
     return (
-        <TokenHandler>
-            <Section1 />
-            <Section2 />
-            <Section3 />
-        </TokenHandler>
+        <Suspense fallback={<div>Chargement...</div>}>
+            <TokenHandler>
+                <Section1 />
+                <Section2 />
+                <Section3 />
+            </TokenHandler>
+        </Suspense>
     );
 }
